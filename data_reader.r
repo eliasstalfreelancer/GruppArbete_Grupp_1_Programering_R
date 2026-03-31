2110b79a7e8ae65e5ff7d9765c2c2ccbc91df4e6
polecat69-jl <info@jldigital.se>
2026-03-31 16:39:43 +0200

    EDA

--- data_reader.r
+++ data_reader.r
@@ -1,1 +1,14 @@
+library("tidyverse")
 
+ecommerce_orders <- read_csv("data/ecommerce_orders.csv")
+
+view(ecommerce_orders)
+
+glimpse(ecommerce_orders)
+summary(ecommerce_orders)
+
+colSums(is.na(ecommerce_orders))
+
+colSums(is.na(ecommerce_orders)) %>% 
+  sort(decreasing = TRUE) %>% 
+  .[. > 0]
